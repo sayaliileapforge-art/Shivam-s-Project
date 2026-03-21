bc1a4dd6dcd00ff1293492dd163556cbf9aed53d
Sayali Gaikar <gaikarsayali1430@gmail.com>
2026-03-21 18:11:26 +0530

    fix: sidebar menu scrolling - add min-h-0 to ScrollArea

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -123,7 +123,7 @@ export function Layout() {
           </div>
 
           {/* Navigation */}
-          <ScrollArea className="flex-1 px-2 lg:px-3 py-4">
+          <ScrollArea className="flex-1 min-h-0 px-2 lg:px-3 py-4">
             <nav className="space-y-1">
               <TooltipProvider>
                 {navigation.map((item) => {
